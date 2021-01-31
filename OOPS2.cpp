4f55299f0e0e960e75f369619b2472e8544adca7
Khali851999 <akshatjindal215@gmail.com>
2021-01-31 21:10:38 +0530

    Init

--- OOPS2.cpp
+++ OOPS2.cpp
@@ -1,18 +1,20 @@
+//preprocessor directives
 #include<iostream>
+#define pi 3.14
 using namespace std;
 class complex{
     public:
     int real = 0;
     int imag = 0;
 
-    int* ptr = nullptr;
+    // int* ptr = nullptr;
 
     complex(){
         real = 0;
         imag = 0;
 
         //initialize memory
-        ptr = new int;
+        // ptr = new int;
     }
 
     complex(int real, int imag){
@@ -27,30 +29,20 @@ class complex{
 
     bool isEqual(complex other){
 
-        if(real == other.real && imag == other.imag){
+        if(this->real == other.real && this->imag == other.imag){
             return true;
         }
 
         return false;
     }
 
-    ~complex(){
-        cout<<"Destructor called"<<endl;
+    // ~complex(){
+    //     cout<<"Destructor called"<<endl;
 
-        //memory leakage
-        delete ptr;
-    }
+    //     //memory leakage
+    //     // delete ptr;
+    // }
 };
-class real{
-    public:
-    int r = 0;
-
-    real(int r){
-        this->r = r;
-    }
-};
-
-
 //predeclaration
 class Box2;
 class Box {
@@ -78,6 +70,30 @@ bool isEqual(Box b1, Box2 b2){
     return false;
 }
 
+class BankAccount{
+    public:
+    double amt = 0;
+    static int roi;
+    static int count;
+
+    BankAccount(){
+        count++;
+    }
+
+    double totalamt(){
+        amt = amt+(amt*roi/100);
+        return amt;
+    }
+
+    static void print(){
+        cout<<count<<endl;
+    }
+
+    
+};
+
+int BankAccount::count = 0;
+int BankAccount::roi = 0;
 int main(){
 
     //5+3i
@@ -100,6 +116,24 @@ int main(){
    b2.setWidth(10.0);
 
    cout<<"isEqual: "<<isEqual(b1, b2);
+
+   Box* ptrBox = &b1;
+   b1.setWidth(30.0);
+   ptrBox->setWidth(20.0);
+   cout<<endl;
+
+   BankAccount a1, a2, a3;
+   BankAccount::roi = 2;
+   a1.amt  = 10;
+   a2.amt = 20;
+
+   cout<<"size is: "<<sizeof(a1)<<endl;
+
+   cout<<"count of account is:"<<BankAccount::count<<endl;
+
+   cout<<a1.totalamt();
+
+   BankAccount::print();
    
    return 0;
 }
