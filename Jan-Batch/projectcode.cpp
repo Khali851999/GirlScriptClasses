eb416cdb642f80829321947aecfd51192535ca00
Khali851999 <akshatjindal215@gmail.com>
2021-02-11 13:56:15 +0530

    Project

---
+++ Jan-Batch/projectcode.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+unordered_map<int,Account>
+class Account{
+    private:
+    int accNo;
+    double balance;
+    string name;
+
+    public:
+    Account(int accno, double balance, string name){
+        this->accNo = accno;
+        this->balance = balance;
+        this->name = name;
+    }
+
+    void open(){
+
+    }
+
+    double enquire(){
+
+    }
+
+    void deposit(){
+
+    }
+
+    void withdrawal(){
+
+    }
+};
+int main(){
+
+}
